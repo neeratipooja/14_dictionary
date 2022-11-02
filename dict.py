cf6684f0288f862a666842809243ca32a0a60f7e
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-02 18:41:32 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,15 @@
+"""
+DAY:wednesday
+DATE:2nd nov 2022
+TOPIC:dictionary,set
+AUTHOR:pooja
+"""
+a={1:'pooja'}
+print(type(a)) #dict
+print(a[1]) #pooja
+b={'p':'banu'} 
+print(b['p']) #banu
+a={'hey','hi','hello'}
+print(a) #'hi','hello','hey'
+a.add('kiran')
+print(a) #'kiran','hey','hello','hi'
